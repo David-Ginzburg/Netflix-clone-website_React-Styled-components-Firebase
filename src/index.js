38e14e6b31ebe956c98558e8df19fa5f94f2fd0d
Vadim Mostovenko <vadim.mostovenko@gmail.com>
2021-04-10 22:01:23 +0300

    Firebase init

--- src/index.js
+++ src/index.js
@@ -2,12 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { GlobalStyles } from './global-styles'
-import App from './app';
+import { App } from './app';
+import { FirebaseContext } from './context/firebase'
+
+const { VITE_API_KEY, VITE_APP_ID, VITE_AUTH_DOMAIN, VITE_DATABASE_URL, VITE_MESSAGING_SENDER_ID, VITE_PROJECT_ID, VITE_STORAGE_BUCKET } = import.meta.env
+
+const config = {
+    apiKey: VITE_API_KEY,
+    authDomain: VITE_AUTH_DOMAIN,
+    databaseURL: VITE_DATABASE_URL,
+    projectId: VITE_PROJECT_ID,
+    storageBucket: VITE_STORAGE_BUCKET,
+    messagingSenderId: VITE_MESSAGING_SENDER_ID,
+    appId: VITE_APP_ID
+}
+
+const firebase = window.firebase.initializeApp(config)
 
 ReactDOM.render(
     <>
-        <GlobalStyles />
-        <App />
+        <FirebaseContext.Provider value={{ firebase: window.firebase }}>
+            <GlobalStyles />
+            <App />
+        </FirebaseContext.Provider>
     </>,
     document.getElementById('root')
 );
