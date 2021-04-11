f6090d36a1fef1566e6744939d84c227a8bce63b
Vadim Mostovenko <vadim.mostovenko@gmail.com>
2021-04-11 21:22:47 +0300

    SignIn functionality

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,9 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import { useHistory } from 'react-router-dom'
 
 import { HeaderContainer, FooterContainer } from '../containers'
 import { Form } from '../components'
+import { FirebaseContext } from '../context/firebase'
+import * as ROUTES from '../constants/routes'
 
 export default function Signin() {
+    const history = useHistory()
+    const { firebase } = useContext(FirebaseContext)
     const [error, setError] = useState('')
     const [emailAddress, setEmailAddress] = useState('')
     const [password, setPassword] = useState('')
@@ -12,43 +17,53 @@ export default function Signin() {
 
     const handleSignin = (event) => {
         event.preventDefault()
+
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(emailAddress, password)
+            .then(() => {
+                setEmailAddress('');
+                setPassword('');
+                setError('');
+                history.push(ROUTES.BROWSE);
+            }).catch((error) => setError(error.message))
     }
 
     return (
         <>
-        <HeaderContainer>
-            <Form>
-                <Form.Title>Sign In</Form.Title>
-                {error && <Form.Error>{error}</Form.Error>}
-                
-                <Form.Base onSubmit={handleSignin} method="POST">
-                    <Form.Input
-                        type="email"
-                        placeholder="Email address"
-                        value={emailAddress}
-                        onChange={({ target }) => setEmailAddress(target.value)}
-                    />
-                    <Form.Input 
-                        type="password"
-                        placeholder="Password"
-                        value={password}
-                        autoComplete="off"
-                        onChange={({ target }) => setPassword(target.value)}
-                    />
-                    <Form.Submit disabled={isValid} type="Submit">
-                        Sign In
-                    </Form.Submit>
+            <HeaderContainer>
+                <Form>
+                    <Form.Title>Sign In</Form.Title>
+                    {error && <Form.Error>{error}</Form.Error>}
+                    
+                    <Form.Base onSubmit={handleSignin} method="POST">
+                        <Form.Input
+                            type="email"
+                            placeholder="Email address"
+                            value={emailAddress}
+                            onChange={({ target }) => setEmailAddress(target.value)}
+                        />
+                        <Form.Input 
+                            type="password"
+                            placeholder="Password"
+                            value={password}
+                            autoComplete="off"
+                            onChange={({ target }) => setPassword(target.value)}
+                        />
+                        <Form.Submit disabled={isValid} type="Submit">
+                            Sign In
+                        </Form.Submit>
 
-                    <Form.Text>
-                        New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
-                    </Form.Text>
-                    <Form.TextSmall>
-                        This page is protected by Google reCAPTCHA.
-                    </Form.TextSmall>
-                </Form.Base>
-            </Form>
-        </HeaderContainer>
-        <FooterContainer />
+                        <Form.Text>
+                            New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+                        </Form.Text>
+                        <Form.TextSmall>
+                            This page is protected by Google reCAPTCHA.
+                        </Form.TextSmall>
+                    </Form.Base>
+                </Form>
+            </HeaderContainer>
+            <FooterContainer />
         </>
     )
 }
